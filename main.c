ea26d6711087a666d0f159f3d81344546c964d3c
Grazielle Tomaz Neves <105025145+grazielleneves@users.noreply.github.com>
2022-11-17 18:31:54 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+//Faça um programa em C que leia um número N de elementos de um vetor de inteiros V, e em seguida leia os N elementos de V.
+//Após isso, leia um número inteiro K, seguido da leitura de K inteiros. 
+//O seu programa deverá busca todos os K números no vetor V, e escrever 1 se o elemento foi encontrado em V e 0 caso contrário.
+
+void main(){
+    int n, i, j, k;
+    scanf("%d", &n);
+    int v[n];
+    for(i = 0; i < n; i++){
+        scanf("%d", &v[i]);
+    }
+    scanf("%d\n", &k);
+    //int vb[k];
+    int num;
+    for(j = 0; j < k; j++){
+        scanf("%d", &num);
+        busca(v, num, n);
+    }
+    
+}
+int busca(int *v, int num, int n){
+    int i, verifica = 0;
+    for(i = 0; i < n; i++){
+        if(num==v[i]){
+           verifica = 1; 
+        }
+        /*else{
+            printf("0\n");
+        }*/
+    }
+    if(verifica==1){
+        printf("1\n");
+    }
+    else{
+        printf("0\n");
+    }
+}
