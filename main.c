e605d21a87d7376f81a28a0403a0daec3c32a5a7
pwdrc <98365041+pwdrc@users.noreply.github.com>
2022-11-19 17:01:40 -0300

    18/24
    18/24

--- main.c
+++ main.c
@@ -1,39 +1,121 @@
 #include<stdio.h>
+#include <stdlib.h>
 //Faça um programa em C que leia um número N de elementos de um vetor de inteiros V, e em seguida leia os N elementos de V.
 //Após isso, leia um número inteiro K, seguido da leitura de K inteiros. 
 //O seu programa deverá busca todos os K números no vetor V, e escrever 1 se o elemento foi encontrado em V e 0 caso contrário.
-void busca(int *v, int num, int n);
+int busca(int *a, int n, int x);
+void merge(int *arr, int l, int m, int r);
+void mergeSort(int *arr, int l, int r);
 
 int main(){
     int n, i, j, k;
     scanf("%d", &n);
-    int v[n];
+
+    int *v = (int *) malloc(n * sizeof(int));
     for(i = 0; i < n; i++){
         scanf("%d", &v[i]);
     }
+    
+    mergeSort(v, 0, n-1);
     scanf("%d\n", &k);
-    //int vb[k];
-    int num;
+    int *vk = (int *) malloc(k * sizeof(int));
+    for(i = 0; i < n; i++){
+            scanf("%d", &vk[i]);
+        }
+        
+    //int aux;
     for(j = 0; j < k; j++){
-        scanf("%d", &num);
-        busca(v, num, n);
+       	//aux = v[j];
+       	int resultado = busca(v, n, vk[j]);
+        printf("%d\n", resultado);
     }
+
+    return 0;
     
 }
-void busca(int *v, int num, int n){
-    int i, verifica = 0;
-    for(i = 0; i < n; i++){
-        if(num==v[i]){
-           verifica = 1; 
+
+void merge(int *arr, int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    /* create temp arrays */
+    int L[n1], R[n2];
+ 
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
         }
-        /*else{
-            printf("0\n");
-        }*/
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
     }
-    if(verifica==1){
-        printf("1\n");
+ 
+    /* Copy the remaining elements of L[], if there
+    are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
     }
-    else{
-        printf("0\n");
+ 
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
     }
 }
+
+void mergeSort(int *arr, int l, int r)
+{
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l + (r - l) / 2;
+ 
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+ 
+        merge(arr, l, m, r);
+    }
+}
+
+int busca(int* a, int n, int x) { // vetor ordenado
+    int c = 0;
+    int f = n - 1;
+    int m;
+    //int verifica = 0;
+    while (c <= f)
+    {
+        m = (c + f) / 2;
+        if (x == a[m])
+            return 1;
+        else if (x < a[m])
+        {
+            f = m - 1;
+        }
+        else
+        { // x > a[m]
+            c = m + 1;
+        }
+    }
+    return 0;
+}
+
